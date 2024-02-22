9545eb98a761907b1d3eb6c0adbb46d5cb3e7399
Keisyeliz <158522128+Keisyeliz@users.noreply.github.com>
2024-02-22 17:41:19 -0500

    Add files via upload

---
+++ ejemplo.py
@@ -0,0 +1,7 @@
+nota1Flt = float(input('ingrese la nota ->'))
+nota2Flt = float(input('ingrese la nota ->'))
+nota3Flt = float (input('ingrese la nota ->'))
+
+
+definitivaFlt = (nota1Flt + nota2Flt + nota3Flt) / 3
+print('su nota definitiva es:  ',definitivaFlt)
